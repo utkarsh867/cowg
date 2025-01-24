65064e51f057c1f487344e2d7fd4d9f58f70fb6d
Utkarsh Goel <hi@utkarshgoel.dev>
2025-01-25 02:25:25 +0800

    Implement print devices

--- main.go
+++ main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	wg "github.com/utkarsh867/cowg/pkg"
 	"log"
 
 	"golang.zx2c4.com/wireguard/wgctrl"
 )
 
 func main() {
-  client, err := wgctrl.New()
-  if err != nil {
-    log.Fatal(err)
-  }
+	client, err := wgctrl.New()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  devices, err := client.Devices()
-  if err != nil {
-    log.Fatal(err)
-  }
+	devices, err := client.Devices()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  log.Printf("Devices %d", len(devices))
+	wg.PrintDevices(devices)
+
+	log.Printf("Devices %d", len(devices))
 }
-
---
+++ pkg/wg.go
@@ -0,0 +1,13 @@
+package wg
+
+import (
+	"log"
+
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+)
+
+func PrintDevices(devices []*wgtypes.Device) {
+  for _, d := range devices {
+    log.Printf("Device %s of type %s is listening on %d", d.Name, d.Type.String(), d.ListenPort)
+  }
+}
